336c05d5c69c07ed958610839bb922ac29ae3589
SILVERBACKGOD <121541003+Silvrbckw@users.noreply.github.com>
2025-12-17 05:41:34 -0500

    Update site metadata and plugin configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    title: `RCS Gemini Dapp Companion`,
+    description: `Global Web3 Retail & Social for Africa-South1.`,
+    siteUrl: `https://rcs-gemini-dapp.web.app`, // Use your Firebase URL here
+    author: `RCS DISTRIBUTION AND RETAIL, LLC`,
+    region: `Africa-South1`,
+    currencies: [`ZAR`, `NGN`, `KES`, `BRL`, `ARS`, `JMD`]
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Sydney Ecommerce Theme`,
-        short_name: `Sydney`,
+        name: `RCS Gemini Dapp`,
+        short_name: `RCS Dapp`,
         start_url: `/`,
         background_color: `#000000`,
-        theme_color: `#ffffff`,
+        theme_color: `#00ffcc`, // Your Web3 Teal
         display: `standalone`,
-        icon: 'src/assets/favicon.png',
+        icon: 'src/assets/favicon.png', 
       },
     },
+    'gatsby-plugin-ipfs',
     'gatsby-plugin-netlify',
   ],
 };
